846efa7fc0d64b358fb5e023a50a9ca29e8c13bb
CCXU <ccxu09@yeah.net>
2024-04-01 16:03:53 +0800

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,9 @@
 let express = require('express');
 let app = express();
 
-console.log("Hello  World");
+
+app.get('/', (req, res) => res.send('Hello World'))
+app.listen(3000, () => console.log('Hello World'))
 
 
 
